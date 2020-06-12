61293fb6804f21956068429187a3d976070010a6
piaic65617 <fashraf030@gmail.com>
2020-06-12 18:52:04 +0500

    updated app.js an created Room.js

---
+++ src/Room.js
@@ -0,0 +1,37 @@
+import React, { useState } from 'react';
+
+function Room() {
+  // const faizan = useState(true);  
+  // console.log("ssfs = ",faizan); 
+
+  const [isLit, setLit] = useState(false);
+  let [Age, setAge] = useState(45);
+
+  // const updateLit = function (){
+  //   console.log("Button Clicked");
+  //   setLit(!isLit);
+  // }
+  // function increaseAge(){
+  //   console.log("Age Incresed");
+  //   setAge(++isAge);
+  // }
+  return (
+     <div>
+          This room is : {isLit? "Lit": "Dark"}
+          <br /><br />
+          <button onClick = {()=> setLit(!isLit)}>Toggle Button</button>
+          <br /><br />
+           Age: {Age}
+           <br /><br />
+          <button onClick = {()=> {
+                            console.log("Increase age arrow function");
+                            setAge(++Age);
+                            }}>
+              Increase Age
+          </button>
+     </div>
+  );
+}
+export default Room;
+
+
